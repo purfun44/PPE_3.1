0f39c4bb8af8d3e0e2c13a96d2292cf71b51c355
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-26 18:58:53 +0200

    Create v_personne_recherchee.php

---
+++ v_personne_recherchee.php
@@ -0,0 +1,4 @@
+<div class="personne_dans_liste">
+	<p><?php print($personneRecherchee["prenom"]." ".$personneRecherchee["nom"]); ?></p>
+	<p class="afficher_visites" nom="<?php print($personneRecherchee["nom"]) ?>" prenom="<?php print($personneRecherchee["prenom"]); ?>">Afficher ses visites</p>
+</div>
