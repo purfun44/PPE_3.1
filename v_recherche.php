5318903915afa3aba434aabe7c9338c7b5dbc8a5
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-26 18:58:19 +0200

    Create v_recherche.php

---
+++ v_recherche.php
@@ -0,0 +1,11 @@
+<div id="contenu_recherche">
+	<div id="bloc_recherche">
+		<p>Recherchez la personne que vous désirez</p>
+		<input type="text" id="champ_recherche" />
+		<p id="affichage_chaine_recherche"></p>
+	</div>
+	<div id="resultats_recherche">
+	</div>
+	<div id="visites">
+	</div>
+</div>
