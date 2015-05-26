c3a120aa647392e4ae40263154061033f3481b8c
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-26 18:57:29 +0200

    Create v_suppression_praticiens.php

---
+++ v_suppression_praticiens.php
@@ -0,0 +1,7 @@
+<div>
+	<label for="nom">Nom du praticien : </label><input type="text" id="nom" />
+	<label for="prenom">Prénom du praticien : </label><input type="text" id="prenom" />
+	<button type="button" width="20%" id="envoie">Valider</button>
+	<br>
+	<p id="informations"></p>
+</div>
