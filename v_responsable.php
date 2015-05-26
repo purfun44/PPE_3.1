5274d227943eb40cc8fb665fe8b9208d57810706
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-26 18:58:05 +0200

    Create v_responsable.php

---
+++ v_responsable.php
@@ -0,0 +1,18 @@
+<div>
+	<ul>
+		<p>Visiteur : <?php print($_SESSION["prenom"]." ".$_SESSION["nom"]); ?></p>
+		<br>
+		<li>
+			<a href="index.php?uc=gererPraticiens&action=recherchePersonnes">Recherche de visiteurs</a>
+		</li>
+		<li>
+			<a href="index.php?uc=gererPraticiens&action=modificationAffectations">Modifier les affectations</a>
+		</li>
+		<li>
+			<a href="index.php?uc=gererPraticiens&action=modificationPraticiens">Supprimer des praticiens</a>
+		</li>
+		<li>
+			<a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+		</li>
+	</ul>
+</div>
